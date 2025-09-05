9fd2cfd371e65dcb3033671e4c93e36c384261ca
mcrucifix <michel.crucifix@uclouvain.be>
2025-09-05 20:12:33 +0200

    quick and dirty add complex periodogram

--- R/periodogram.R
+++ R/periodogram.R
@@ -23,9 +23,40 @@ periodogram <- function(xdata){
   return(OUT)
 }
 
+#' @export periodogram_complex
+periodogram_complex <- function(xdata){
+  xdata = stats::as.ts(xdata)
+  dt = deltat(xdata)
+  startx = stats::start(xdata)[1]
+  N <- length(xdata)
+  N1 <- N-1
+  freqs <- ((seq(N)-1)/dt/N)[0:N1]
+  f <-  fft(xdata)[0:N1]
+
+  negatives <- which(freqs > 1/(2*dt))
+  freqs[negatives] = freqs[negatives] - 1./dt
+  O <- order(freqs)
+  freqs <- freqs[O]
+  f <- f[O]
+   
+  Power <- Mod(f)^2 / (dt)^2
+  Phase <- Arg(f)
+  OUT <- list(Freq=freqs, Power=Power, Phase=Phase)
+  attr(OUT, "class") = "periodogram_complex"
+  return(OUT)
+}
+
+
+
 #' @rdname periodogram
 #' @export
 plot.periodogram <- function(X,log='xy', xlabel= "Frequency", ylabel="Power density", ...){
+  plot(X$Freq, X$Power, type='l', log=log,xlabel=xlabel, ylabel=ylabel, ...)
+}
+
+#' @rdname periodogram_complex
+#' @export
+plot.periodogram_complex <- function(X,log='y', xlabel= "Frequency", ylabel="Power density", ...){
   plot(X$Freq, X$Power, type='l', log=log,xlabel=xlabel, ylabel=ylabel, ...)
 }
 
