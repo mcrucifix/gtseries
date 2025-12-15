7543c88977b99edb513d094238b9aebfb147aeed
mcrucifix <michel.crucifix@uclouvain.be>
2025-12-15 12:54:13 +0100

    haar modif

--- R/haar.R
+++ R/haar.R
@@ -1,13 +1,8 @@
-# TO DO: THERE IS A BIAS FOR THE LONG LENGHTSCALES
-# I GUESS MUST MULTIPLY BY (N-1)/N where N is
-# length / scale
-
-
-
 #' Haar fluctuation spectrum
 #'
 #' @description Haar fluctuation spectrum after Lovejoy
 #' still needs to be documented
+#' see also parcival and walden wavelets book
 #' currently requires n=2^i data points
 #' @param x the input series (typically a numeric)
 #' @param q the fluctuation order
@@ -25,14 +20,23 @@ haar <- function(x, q = 2, discarded_scales = 4) {
   DT <- sapply(seq(nsteps), function(i) {
     m <- 2^i
     nn <- nmax / m
+    # organises the series into m columns
+    # e.g. for scale 4, we will do 8 columns
     M <- matrix(x, nmax / m, m, byrow = TRUE)
+    # take the average over the first m/2 columns
     M1 <- M[, 1:(m / 2), drop = FALSE]
-    M2 <- M[, (m / 2 + 1):m, drop = FALSE]
     tM1 <- apply(M1, 1, mean)
+    # take the average over last m/2 columns
+    M2 <- M[, (m / 2 + 1):m, drop = FALSE]
     tM2 <- apply(M2, 1, mean)
-    abs(tM2 - tM1) * (2 * nn) / (2 * nn - 1)
+    # take the difference : this effectively
+    # computes the convolution by a Haar wavelet of 
+    # scale m/2, over successive steps spaced by m
+    abs(tM2 - tM1)
   })
   x <- 2^(seq(nsteps)-1)
+  # y is the _mean_ convolution of the signal by the Haar 
+  # wavelet
   y <- sapply(DT, function(x) mean(x^q))
   H <- data.frame(x = x, y = y, logx = log(x), logy = log(y))
   kept_scales <- seq(max(2, nsteps - discarded_scales))
